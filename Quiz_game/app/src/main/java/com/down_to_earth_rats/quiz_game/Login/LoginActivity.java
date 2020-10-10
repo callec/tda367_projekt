187ab99143065d961fc7eaa6d5aa9c0432bcd306
Erik Blomberg <31245545+RedSpettan@users.noreply.github.com>
2020-10-10 17:25:20 +0200

    The Activity can now access the different View components. Ready to implement the rest!

--- Quiz_game/app/src/main/java/com/down_to_earth_rats/quiz_game/Login/LoginActivity.java
+++ Quiz_game/app/src/main/java/com/down_to_earth_rats/quiz_game/Login/LoginActivity.java
@@ -5,6 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.down_to_earth_rats.quiz_game.R;
+import com.down_to_earth_rats.quiz_game.databinding.ActivityLoginBinding;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+/**
+ * Created by Erik Blomberg
+ *
+ *
+ */
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,7 +22,17 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_login);
+        ActivityLoginBinding binding = ActivityLoginBinding.inflate(getLayoutInflater());
+
+        TextInputLayout usernameInputLayout = binding.usernameInputLayout;
+        TextInputEditText usernameInputEdit = binding.usernameInputEdit;
+
+        TextInputLayout passwordInputLayout = binding.passwordInputLayout;
+        TextInputEditText passwordInputEdit = binding.passwordInputEdit;
+
+        MaterialButton loginButton = binding.loginButton;
+
+        setContentView(binding.getRoot());
     }
 
 }
